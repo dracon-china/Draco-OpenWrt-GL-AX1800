e27ac609bdde074f7e82fc9f284c1083e8e0c2c7
muzi502 <muzi502.li@gmail.com>
2023-08-04 20:20:53 +0800

    fixes build error

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,28 +1,14 @@
 #!/bin/bash
 PWD=$(pwd)
 
-echo "请选择构建设备："
-echo "1. AX1800"
-echo "2. AXT1800"
-read input
-
-case $input in
-1)
-		echo "构建AX1800"
-		DEVICE="ax1800"
-		;;
-2)
-		echo "构建AXT1800"
-		DEVICE="axt1800"
-		;;
-esac
+DEVICE="axt1800"
 
 #clone source tree
 git clone https://github.com/gl-inet/gl-infra-builder.git $PWD/gl-infra-builder
 cp -r $PWD/*.yml $PWD/gl-infra-builder/profiles
 cd $PWD/gl-infra-builder
 #setup
-python3 setup.py -c config-wlan-ap-5.4.yml
+python3 setup.py -c configs/config-wlan-ap-5.4.yml
 
 cd wlan-ap/openwrt
 ./scripts/gen_config.py  $PWD/gl-infra-builder/profiles/glinet_$DEVICE glinet_depends
